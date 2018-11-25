2f0746f805a9847e0f9bd2f8eb628f0f03085f05
Daria <erika-vkn@yandex.ru>
2018-11-25 07:23:45 +0300

    Домашнее задание к уроку 2

--- Java1homework/src/HomeworkL2.java
+++ Java1homework/src/HomeworkL2.java
@@ -1,37 +1,48 @@
 import java.util.Arrays;
-import java.util.Random;
 
 /**
  * Java 1 Lesson 2 homework
  *
  * @author Daria Nikolaeva
- * @version dated Nov 24, 2018
+ * @version dated Nov 25, 2018
  *
  */
 
-
 public class HomeworkL2 {
     public static void main (String[] args) {
-         changeArrayOneZero();
-         setArrayEight();
-         modifyArray();
-         findMinMax();
 
-         // Task 6
-         int[] arr6 = {1,1,1,1,4};
-         System.out.println(checkBalance(arr6));
-         int[] arr61 = {1,1,1,1,5};
-         System.out.println(checkBalance(arr61));
+        // Task 1
+        int[] arr1 = {1, 1, 0, 0, 0, 1, 0, 1, 0};
+        System.out.print("Новый массив из задания 1: ");
+        printArray(changeArrayOneZero(arr1));
 
-         // Task 7
-         int[] arr7 = {1, 2, 33, -5, 28, 12, 78, 99, 105, -60, 15, -2, 200};
-         int n = 100;
-         //System.out.println(Arrays.toString(moveArray(arr7, n)));
+        //Task 2
+        int[] arr2 = new int[8];
+        System.out.print("Массив из задания 2: ");
+        printArray(fillArrayEight(arr2));
 
+        //Task 3
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.print("Новый массив из задания 3: ");printArray(modifyArray(arr3));
+
+        //Task 4
+        int n = 10;
+        int[][] arr4 = new int[n][n];
+        System.out.println("Двумерный массив задание 4");
+        print2DArray(fillArrayOne(arr4));
+
+        //Task 5
+        int[] arr5 = {50,77,89,105,2,-5,900,0};
+        findMinMax(arr5);
+
+        // Task 6
+        int[] arr6 = {1,1,1,1,4};
+        System.out.println("Суммы левой и правой части равны: " + checkBalance(arr6));
+        int[] arr61 = {1,1,1,1,6};
+        System.out.println("Суммы левой и правой части равны: " + checkBalance(arr61));
     }
 
     public static int[] changeArrayOneZero(int[] arr1) {
-        int[] arr1 = {1, 1, 0, 0, 0, 1, 0, 1, 0};
         for (int i = 0; i < arr1.length; i++) {
             if (arr1[i] == 1) {
                 arr1[i] = 0;
@@ -39,33 +50,36 @@ public class HomeworkL2 {
                 arr1[i] = 1;
             }
         }
-        //
+        return arr1;
     }
 
-    public static void setArrayEight() {
-        int[] arr2 = new int[8];System.out.println(Arrays.toString(arr1));
+    public static int[] fillArrayEight(int[] arr2) {
         for (int i = 0, j = 0; i < arr2.length; i++, j += 3) {
             arr2[i] = 0 + j;
         }
-        //System.out.println(Arrays.toString(arr2));
+        return arr2;
     }
 
-    public static void modifyArray() {
-        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    public static int[] modifyArray(int[] arr3) {
         for (int i = 0; i < arr3.length; i++) {
             if (arr3[i] < 6) {
                 arr3[i] = arr3[i] * 2;
             }
         }
-        //System.out.println(Arrays.toString(arr3));
+        return arr3;
     }
 
-    public static void findMinMax() {
-        int[] arr5 = new int[10];
-        Random ran = new Random();
-        for (int i = 0; i < arr5.length; i++) {
-            arr5[i] = ran.nextInt(101);
+    public static int[][] fillArrayOne(int[][] arr4) {
+        for (int i = 0, j = 0; i < arr4[0].length; i++, j++) {
+            arr4[i][j] = 1;
         }
+        for (int i = 0, j = arr4[1].length-1; i <arr4[0].length; i++, j--) {
+            arr4[i][j] = 1;
+        }
+        return arr4;
+    }
+
+    public static void findMinMax(int[] arr5) {
         int min = arr5[0];
         int max = arr5[0];
         for (int j = 0; j < arr5.length; j++) {
@@ -76,15 +90,14 @@ public class HomeworkL2 {
                 max = arr5[j];
             }
         }
-        //System.out.println(Arrays.toString(arr5));
-        //System.out.println("min " + min);
-        //System.out.println("max " + max);
+        System.out.println("min " + min);
+        System.out.println("max " + max);
     }
 
     public static boolean checkBalance(int[] arr6) {
         int sum = 0;
-        for (int i = 0; i < arr6.length; i++) {
-            sum = sum + arr6[i];
+        for (int i : arr6) {
+            sum += i;
         }
         int result = 0;
         if (sum % 2 !=0) {
@@ -105,14 +118,15 @@ public class HomeworkL2 {
         return result > 0;
     }
 
-    public static int[] moveArray(int[] arr7, int n) {
-        for (int i = 0; i < arr7.length; i++) {
-            arr7[i] = arr7[i] + n;
-        }
-        return arr7;
-    }
-
     public static void printArray(int[] arr) {
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void print2DArray(int[][] arr) {
+        for (int i = 0; i<arr[0].length; i++) {
+            for (int j = 0; j <arr[1].length; j++)
+                System.out.print(arr[i][j] + " ");
+                System.out.println();
+        }
     }
 }
